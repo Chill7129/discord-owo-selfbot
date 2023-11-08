9b1400c21a150354090b75c7fdfa55ada13affdd
LeraN <133788550+Pangleran@users.noreply.github.com>
2023-11-09 04:36:27 +0800

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,12 +7,12 @@ export default {
         let document = ""
         const listCommand = Object.keys(global.commands)
         for(const command of listCommand) document += `**${command}:** ${global.commands[command].info}\n`
-        document += "Join Our Support Server For Help: https://discord.gg/Yr92g5Zx3e"
+        document += ":) Bee Your Self"
         if(args[0]) message.reply(
             listCommand.includes(args[0]) 
             ? `**${args[0]}:** ${global.commands[args[0]].info}`
             : "Command Not Found!"
         )
-        else message.reply(document)
+        else message.channel.send(document)
     }
 }
